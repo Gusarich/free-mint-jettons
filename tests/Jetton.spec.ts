fca486a37732d745fb033723420d9c56c300008c
Gusarich <danielsedovzzz@gmail.com>
2023-12-16 10:59:53 +0300

    upload default jetton contract

---
+++ tests/Jetton.spec.ts
@@ -0,0 +1,40 @@
+import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
+import { Cell, toNano } from '@ton/core';
+import { JettonMinter } from '../wrappers/JettonMinter';
+import { JettonWallet } from '../wrappers/JettonWallet';
+import '@ton/test-utils';
+import { compile } from '@ton/blueprint';
+
+describe('Qwe', () => {
+    let code: Cell;
+
+    beforeAll(async () => {
+        code = await compile('JettonMinter');
+    });
+
+    let blockchain: Blockchain;
+    let deployer: SandboxContract<TreasuryContract>;
+    let jettonMinter: SandboxContract<JettonMinter>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+
+        jettonMinter = blockchain.openContract(JettonMinter.createFromConfig({}, code));
+
+        deployer = await blockchain.treasury('deployer');
+
+        const deployResult = await jettonMinter.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: jettonMinter.address,
+            deploy: true,
+            success: true,
+        });
+    });
+
+    it('should deploy', async () => {
+        // the check is done inside beforeEach
+        // blockchain and jettonMinter are ready to use
+    });
+});
